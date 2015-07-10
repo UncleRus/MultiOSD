6594ce5a2cb226c3ab18cfce4a1a1177e04856e0
UncleRus <unclerus@gmail.com>
2015-07-10 15:30:32 +0500

    MAVLink CRC PROGMEM patch

--- firmware/telemetry/mavlink/lib/mavlink_helpers.h
+++ firmware/telemetry/mavlink/lib/mavlink_helpers.h
@@ -249,8 +249,8 @@ MAVLINK_HELPER uint8_t mavlink_frame_char_buffer(mavlink_message_t* rxmsg,
 	*/
 #if MAVLINK_CRC_EXTRA
 #ifndef MAVLINK_MESSAGE_CRC
-	static const uint8_t mavlink_message_crcs[256] = MAVLINK_MESSAGE_CRCS;
-#define MAVLINK_MESSAGE_CRC(msgid) mavlink_message_crcs[msgid]
+	static const uint8_t mavlink_message_crcs[256] PROGMEM = MAVLINK_MESSAGE_CRCS;
+#define MAVLINK_MESSAGE_CRC(msgid) (pgm_read_byte(&mavlink_message_crcs[msgid]))
 #endif
 #endif
 
